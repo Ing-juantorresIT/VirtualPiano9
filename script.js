bc20f24057e2d134f88ac4a1c8a431432eddc552
Ing.Juan Torres <ing.juantorres.it@gmail.com>
2020-10-03 18:50:28 -0400

    Modificacion .js audio

--- script.js
+++ script.js
@@ -14,19 +14,19 @@ function saludo() {
 }
 
 let keys_code = [
-  "A",
-  "S",
-  "D",
-  "F",
-  "G",
-  "H",
-  "J",
-  "W",
-  "E",
+  "a",
+  "s",
+  "d",
+  "f",
+  "g",
+  "h",
+  "j",
+  "w",
+  "e",
   " ", // EL ESPACIO EN BLANCO ES PARA REORDENAR Y HACER CONINCIDIR LAS LETRAS
-  "T",
-  "Y",
-  "U",
+  "t",
+  "y",
+  "u",
 ];
 
 //
@@ -34,15 +34,15 @@ let keys_code = [
 //
 
 document.addEventListener("keypress", function (event) {
-  let busqueda = event.key.toUpperCase(); //Esto es para alamacenar la letra o key que se quiere buscar en el array
+  let busqueda = event.key.toLowerCase(); //Esto es para alamacenar la letra o key que se quiere buscar en el array
   let posicion = keys_code.indexOf(busqueda); //Esto almacenara la posicion de la letra o key dentro del array
   let keyChange = document.querySelectorAll("kbd");
   let keyPo = keyChange[posicion]; //
 
-  if (keys_code.includes(event.key.toUpperCase())) {
+  if (keys_code.includes(busqueda)) {
     //
 
-    let x = new Audio(`resource/${event.key.toUpperCase()}.mp3`); // crear un objeto de audio,
+    let x = new Audio(`resource/${event.key.toLowerCase()}.mp3`); // crear un objeto de audio,
 
     keyPo.classList.add("keyUp"); //Se le pone la clase KeyUp a la letra correcta por que se dertimon gracias a la posicion dentro del array
     return x.play(); //Reproduce el sonido de las letras.
@@ -54,10 +54,10 @@ document.addEventListener("keypress", function (event) {
 });
 
 document.addEventListener("keyup", function (event) {
-  if (keys_code.includes(event.key.toUpperCase())) {
+  if (keys_code.includes(event.key.toLowerCase())) {
     let keyChange = document.querySelectorAll("kbd");
 
-    let busqueda = event.key.toUpperCase();
+    let busqueda = event.key.toLowerCase();
     let posicion = keys_code.indexOf(busqueda);
     let keyPo = keyChange[posicion];
 
